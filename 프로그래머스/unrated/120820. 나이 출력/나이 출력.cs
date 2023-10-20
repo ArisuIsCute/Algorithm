b602a33896e2435e5325460e7739dae5843eb4be
YooYeonJun 'ㅁ <128462625+ArisuIsCute@users.noreply.github.com>
2023-10-20 19:14:58 +0900

    [level unrated] Title: 나이 출력, Time: 0.21 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120820. 나이 출력/나이 출력.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int age) {
+        
+        int year = 2022;
+        
+        int answer = 2022 - age + 1;
+        
+        
+        return answer;
+    }
+}
