4feff651857f0ffb678d7398d81255fe1dde846a
YooYeonJun 'ㅁ <128462625+ArisuIsCute@users.noreply.github.com>
2023-10-20 19:10:18 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.25 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Solution {
+    public string solution(string code) {
+        string answer = "";
+        
+        bool mode = false;
+        
+        for(int i = 0; i < code.Length; i++){
+            
+            if(code[i] == '1'){
+                mode = !mode;
+            }else{
+                if(mode){
+                    if(!(i % 2 == 0)){
+                        answer += code[i]; 
+                    }
+                }else{
+                    if(i % 2 == 0){
+                        answer += code[i];
+                    }
+                }
+            }
+        }
+        if(answer == ""){
+            answer = "EMPTY";
+        }
+        return answer;
+    }
+}
