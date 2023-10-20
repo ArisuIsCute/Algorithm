8bfd2d59eab176a50f263e10deebfab9c73fca04
YooYeonJun 'ㅁ <128462625+ArisuIsCute@users.noreply.github.com>
2023-10-20 19:18:53 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.24 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        
+        for(int i = 0; i < numbers.Length; i++){
+            if(answer > n) break;
+            answer += numbers[i];
+        }
+        
+        return answer;
+    }
+}
