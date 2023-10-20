b54d5dee240305e2af7ce163a5b8c237052a4fd8
YooYeonJun 'ㅁ <128462625+ArisuIsCute@users.noreply.github.com>
2023-10-20 19:02:52 +0900

    [Bronze V] Title: Hello World, Time: 56 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace beakJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
