dd77abb64889ab2fde1cb9d1009e7b2059b05635
YooYeonJun 'ㅁ <128462625+ArisuIsCute@users.noreply.github.com>
2023-10-30 17:12:30 +0900

    [Silver IV] Title: 돌 게임 2, Time: 64 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cs
@@ -0,0 +1,42 @@
+using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace BaekJoon
+{
+    internal class BaekJoon
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool isSk = true;
+
+            while (true)
+            {
+                if(n >= 3)
+                {
+                    n -= 3;
+                }
+                else if(n >= 1)
+                {
+                    n -= 1;
+                }
+
+                if (n == 0) break;
+                isSk = !isSk;
+            }
+            if(!isSk)
+            {
+                Console.WriteLine("SK");
+            }
+            else
+            {
+                Console.WriteLine("CY");
+            }
+        }
+    }
+}
