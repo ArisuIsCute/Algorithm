36329347e861f8c11e1a830ff94ea9202c9eeec8
YooYeonJun 'ㅁ <128462625+ArisuIsCute@users.noreply.github.com>
2023-11-01 19:13:28 +0900

    [level unrated] Title: rny_string, Time: 0.29 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public string solution(string rny_string) {
+        string answer = "";
+        for(int i = 0; i < rny_string.Length; i++)
+        {
+            if(rny_string[i] == 'm') answer += "rn";
+            else answer += rny_string[i];
+        }
+        return answer;
+    }
+}
