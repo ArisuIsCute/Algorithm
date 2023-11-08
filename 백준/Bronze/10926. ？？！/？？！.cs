0b75cbcf868b474f56daceb6eaa462338dea047b
YooYeonJun 'ㅁ <128462625+ArisuIsCute@users.noreply.github.com>
2023-11-08 16:57:31 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 5380 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace BaekJoon
+{
+    internal class BaekJoon
+    {
+        static void Main(string[] args)
+        {
+            string id = ReadLine();
+
+            WriteLine(id + "??!");
+        }
+    }
+}
