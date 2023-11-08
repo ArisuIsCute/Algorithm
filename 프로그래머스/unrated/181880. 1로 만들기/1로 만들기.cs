17eab62b3e9fdfea2e0000974aef2ced781f6d34
YooYeonJun 'ㅁ <128462625+ArisuIsCute@users.noreply.github.com>
2023-11-08 20:05:40 +0900

    [level unrated] Title: 1로 만들기, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int cnt = 0;
+        for(int i = 0; i < num_list.Length; i++){
+            while(num_list[i] != 1){
+                if(num_list[i] % 2 == 0) num_list[i] /= 2;
+                else num_list[i] = (num_list[i] - 1) / 2;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
