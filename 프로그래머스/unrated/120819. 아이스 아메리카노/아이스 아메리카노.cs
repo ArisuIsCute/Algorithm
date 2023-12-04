69fe6c0be81b1df5ff73af7f56bce1c453ba97b7
YooYeonJun 'ㅁ <128462625+ArisuIsCute@users.noreply.github.com>
2023-12-04 16:49:38 +0900

    [level unrated] Title: 아이스 아메리카노, Time: 0.25 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        
+        while(money >= 5500){
+            money -= 5500;
+            answer[0]++;
+        }
+        answer[1] = money;
+        
+        return answer;
+    }
+}
