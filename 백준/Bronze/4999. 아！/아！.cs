8845bf2adc79a36216dc6f16491f8f74e4cff70a
YooYeonJun 'ㅁ <128462625+ArisuIsCute@users.noreply.github.com>
2023-12-11 17:03:52 +0900

    [Bronze V] Title: 아!, Time: 60 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+
+namespace solution
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            if(ReadLine().Length >= ReadLine().Length) WriteLine("go");
+            else WriteLine("no");
+        }
+    }
+}
