19dab3801f2c930a42ff3b03c1c800450528dd04
YooYeonJun 'ㅁ <128462625+ArisuIsCute@users.noreply.github.com>
2023-12-11 17:00:42 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 60 ms, Memory: 5456 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,13 @@
+using System;
+using static System.Console;
+
+namespace solution
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine(ReadLine().Length);
+        }
+    }
+}
